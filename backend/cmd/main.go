49891c46af2dca181ad771d4e67f5e05f09ec8c4
kammiii <kamran.nazir2504@gmail.com>
2025-07-10 00:21:16 +0200

    feat: redo backend structure and run mysql and backend on same network with docker-compose

---
+++ backend/cmd/main.go
@@ -0,0 +1,57 @@
+// @title Sykell Crawler API
+// @version 1.0
+// @description API for crawling and analyzing websites
+// @BasePath /
+// @schemes http
+// @accept json
+// @produce json
+
+// @contact.name Kamran Nazir
+
+// @host localhost:8080
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "github.com/kammiii/backend/docs"
+	"github.com/kammiii/backend/internal/api"
+	"github.com/kammiii/backend/internal/db"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	origins := strings.Split(os.Getenv("CORS_ORIGINS"), ",")
+
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     origins,
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	db.Connect()
+	api.RegisterRoutes(r)
+
+	// Swagger docs endpoint
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(":8080")
+}
