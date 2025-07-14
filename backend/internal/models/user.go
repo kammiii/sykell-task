001d27518f75a3e57582a643412e3feea2834aaa
kaammii <kamrannazir692@gmail.com>
2025-07-14 12:31:25 +0200

    feat: User model

---
+++ backend/internal/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"time"
+)
+
+type User struct {
+	ID        uint      `gorm:"primaryKey" json:"id"`
+	Name      string    `gorm:"unique;not null" json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
